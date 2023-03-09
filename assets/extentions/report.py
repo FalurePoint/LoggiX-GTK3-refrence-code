e037f88ad4fb4cad3cef5879b67562980dfc3a7a
ProjectAlpha <105343814+FalurePoint@users.noreply.github.com>
2023-03-09 14:09:36 -0900

    Added backend to begin buliding debug loging into software

---
+++ assets/extentions/report.py
@@ -0,0 +1,28 @@
+import inspect
+import os
+import datetime
+
+def error(content):
+    local = os.path.abspath(__file__)
+    resources_dir = os.path.dirname(local)
+    resources_dir = os.path.join(*resources_dir.split(os.sep)[:-2])
+    caller_frame = inspect.stack()[1]
+    filename, line_number, function_name, code_line, code_index = inspect.getframeinfo(caller_frame[0])
+    calling_script = os.path.basename(filename)
+    timestamp = datetime.datetime.now()
+    year = timestamp.strftime("%y"[-2:])
+    timestamp = timestamp.strftime("%m%d%H%M%S")
+    timestamp = year + timestamp
+    log_file = "/" + resources_dir + "/log.txt"
+    x = open(str(log_file), "a+")
+    x.write(str(timestamp))
+    x.write(" from ")
+    x.write(calling_script)
+    x.write(" around line ")
+    x.write(str(line_number))
+    x.write(": ")
+    x.write(str(content))
+    x.write("\n")
+    x.close()
+
+
